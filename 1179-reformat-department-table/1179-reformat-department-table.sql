a9ad1c49bbdbfcec69bb3db7fb8245f8ad5f283a
Chanism <48588531+ykdy3951@users.noreply.github.com>
2025-03-09 15:48:38 +0900

    Time: 1109 ms (35.05%), Space: 0B (100%) - LeetHub

---
+++ 1179-reformat-department-table/1179-reformat-department-table.sql
@@ -0,0 +1,19 @@
+/* Write your PL/SQL query statement below */
+SELECT *
+FROM department
+PIVOT (SUM(revenue) Revenue
+    FOR month IN (
+        'Jan' Jan,
+        'Feb' Feb,
+        'Mar' Mar,
+        'Apr' Apr,
+        'May' May,
+        'Jun' Jun,
+        'Jul' Jul,
+        'Aug' Aug,
+        'Sep' Sep,
+        'Oct' Oct,
+        'Nov' Nov,
+        'Dec' Dec
+    )
+)
